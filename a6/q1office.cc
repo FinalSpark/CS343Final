ea7ccdbcd432255f86494890076a16e8363242f4
Fangcun Yu <godomoe@gmail.com>
2013-12-02 23:18:43 -0500

    comments

--- a6/q1office.cc
+++ a6/q1office.cc
@@ -3,6 +3,10 @@
 #include "MPRNG.h"
 #include <vector>
 
+   /**
+    * WATCardOffice::(constructor)
+    * initialize {numCouriers} couriers
+    */
   WATCardOffice::WATCardOffice( Printer &prt, Bank &bank, unsigned int numCouriers ):
         prt(&prt), bank(&bank), numCouriers(numCouriers){
         couriers = new Courier*[numCouriers];
@@ -11,6 +15,12 @@
         }
     }
 
+   /**
+    * WATCardOffice::create
+    * create job of creating new card for student {sid} with {amount} dollars.
+    * Job is queued for couriers in queue<job> jobs.
+    * After job is queued, signal condition
+    */
     WATCard::FWATCard WATCardOffice::create( unsigned int sid, unsigned int amount ){
         Job* job = new Job(sid, amount, new WATCard());
         jobs.push(job);
@@ -18,6 +28,13 @@
         condition.signal();
         return job->result;
     }
+
+   /**
+    * WATCardOffice::transfer
+    * create job of transfer {amount} dollars from student {sid}'s account to watcard.
+    * Job is queued for couriers in queue<job> jobs.
+    * After job is queued, signal condition
+    */
     WATCard::FWATCard WATCardOffice::transfer( unsigned int sid, unsigned int amount, WATCard *card ){
         Job* job = new Job(sid, amount, card);
         jobs.push(job);
@@ -26,7 +43,14 @@
         condition.signal();
         return job->result;
     }
-struct WATCardOffice::Job *WATCardOffice::requestWork(){
+
+   /**
+    * WATCardOffice::requestWork
+    * If office is closed (terminated == true), do not wait, pickup the dummy (terminate) job.
+    * If there are jobs in the queue, do not wait, picku put the job.
+    * Otherwise, wait till there are jobs 
+    */
+    struct WATCardOffice::Job *WATCardOffice::requestWork(){
                 //cout << "here1" << endl;
         while (jobs.size() < 1 && !terminated)
         {
@@ -40,6 +64,14 @@ struct WATCardOffice::Job *WATCardOffice::requestWork(){
         return temp;
     }
 
+   /**
+    * WATCardOffice::main
+    * Accept calls
+    * After destructor is called, remove all queued jobs (no need to complete pending tasks since students are all finished at this point)
+    * Push dummy jobs to the queue to signal couriers to terminate.
+    * Set the office's status to terminated and wake up all tasks that are waiting on condition.
+    * Couriers will pickup the dummy task after being waken up here.
+    */
     void WATCardOffice::main(){
       prt->print(Printer::WATCardOffice, 'S');
       while (true)
@@ -79,7 +111,6 @@ struct WATCardOffice::Job *WATCardOffice::requestWork(){
     }
 
 
-
     WATCardOffice::Courier::Courier(Printer *prt, Bank *bank, unsigned int id, WATCardOffice *office):
         prt(prt), bank(bank), id(id), office(office){
 
@@ -91,6 +122,14 @@ struct WATCardOffice::Job *WATCardOffice::requestWork(){
         prt->print(Printer::Courier, id, 'F');
     }
     
+
+   /**
+    * Courier::main
+    * Loop for jobs.
+    * After getting job, try withdraw money, simulate lost.
+    * If lost, add exception to future, otherwise, deposit money and deliver the card
+    * In either cases, the job is recorded for memory deallocation
+    */
     void WATCardOffice::Courier::main(){
         std::vector<struct WATCardOffice::Job *> doneJobs;
         int count = 0;
@@ -105,14 +144,14 @@ struct WATCardOffice::Job *WATCardOffice::requestWork(){
                     }
                   break; 
                 }
+                prt->print(Printer::Courier, id, 't', job->sid, job->amount);
+                bank->withdraw(job->sid, job->amount);
                 bool lostEh = ran(0, 5) == 0;
                 if (lostEh) {
                     //delete job->card;
                     job->result.exception( new Lost );
                 } else {
                     cout << "courier id: " << job->sid << endl;
-                    prt->print(Printer::Courier, id, 't', job->sid, job->amount);
-                    bank->withdraw(job->sid, job->amount);
 
                     job->card->deposit(job->amount);
                     job->result.delivery(job->card);
